d583f1cdcb67e23db5ebb52480a7d1d5c0d1c8dc
maple-pwn <3079403765@qq.com>
2026-08-08 23:26:54 +0800

    feat: complete oidc browser pkce flow

--- internal/services/oauth/client.go
+++ internal/services/oauth/client.go
@@ -49,6 +49,7 @@ type OAuthTokens struct {
 type TokenExchangeResponse struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
+	IDToken      string `json:"id_token,omitempty"`
 	ExpiresIn    int    `json:"expires_in"`
 	Scope        string `json:"scope"`
 	TokenType    string `json:"token_type"`
